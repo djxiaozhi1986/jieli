3080a153bd2dee91669d9f84474e7b3c78d2a19b
GavinSir <djxiaozhi1986@163.com>
2018-09-13 21:43:28 +0800

    up

--- app/Modules/UserCategorys.php
+++ app/Modules/UserCategorys.php
@@ -18,7 +18,7 @@ class UserCategorys extends Model
 {
     public $table = 'courses_users_categorys';//数据库表名
     //数据库字段，白名单，只能查询此队列中的字段
-    protected $fillable = ['category_id', 'id', 'user_id','p_id'];
-    public $primaryKey = "category_id";//主键
+    protected $fillable = ['id', 'c_id', 'user_id','p_id','created_at'];
+    public $primaryKey = "id";//主键
     public $timestamps = false;//是否自动生成时间戳
 }
