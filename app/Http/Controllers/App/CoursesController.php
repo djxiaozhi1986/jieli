3f2a2b37861235161ffda065e9054ad22b108f60
GavinSir <djxiaozhi1986@163.com>
2018-09-17 21:03:45 +0800

    up

--- app/Http/Controllers/App/CoursesController.php
+++ app/Http/Controllers/App/CoursesController.php
@@ -419,7 +419,7 @@ class CoursesController extends Controller{
             $total = Comments::where('course_id',$request->input('course_id'))->count();
             $overplus = 0;
             $res = Comments::where('course_id',$request->input('course_id'))
-                    ->select('courses_comments.*','praise_count as praise_number',DB::raw('CONCAT("http://118.26.164.109:81/uploads/face/",jl_user.user_face)  as from_user_face'))
+                    ->select('comment_id','course_id', 'parent_id', 'content', 'from_user','from_user_name','to_user','to_user_name','created_at','praise_count as praise_num',DB::raw('CONCAT("http://118.26.164.109:81/uploads/face/",jl_user.user_face)  as from_user_face'))
                     ->leftJoin('user','user.user_id','courses_comments.from_user')
                     ->orderBy('praise_count','desc')->skip(0)->take(5)->get()->toArray();
             foreach ($res as $key=>$value){
@@ -490,13 +490,13 @@ class CoursesController extends Controller{
             $page_number = $request->input('page_number')??5;
             $sql = Comments::where('course_id',$request->input('course_id'))->whereNull('parent_id');
             $total = $sql->count();
-            $list =$sql ->select('courses_comments.*',DB::raw('CONCAT("http://118.26.164.109:81/uploads/face/",jl_user.user_face)  as from_user_face'))
+            $list =$sql ->select('comment_id','course_id', 'parent_id', 'content', 'from_user','from_user_name','to_user','to_user_name','created_at','praise_count as praise_num',DB::raw('CONCAT("http://118.26.164.109:81/uploads/face/",jl_user.user_face)  as from_user_face'))
                 ->leftJoin('user','user.user_id','courses_comments.from_user')
                 ->skip(($page_index - 1) * $page_number)->take($page_number)->get()->toArray();
             //查询列表的回复列表
             foreach($list as $key=>$value){
                 //此评论的点赞数量
-                $list[$key]['praise_num'] = Praises::where('comment_id',$value['comment_id'])->count();
+//                $list[$key]['praise_num'] = Praises::where('comment_id',$value['comment_id'])->count();
                 //当前登录用户是否已经点过赞
                 if($request->input('login_user')){
                     $exits = Praises::where('from_user',$request->input('login_user'))->where('comment_id',$value['comment_id'])->exists();
@@ -532,13 +532,13 @@ class CoursesController extends Controller{
             $page_number = $request->input('page_number')??5;
             $sql = Comments::where('parent_id',$request->input('comment_id'));
             $total = $sql->count();
-            $list =$sql ->select('courses_comments.*',DB::raw('CONCAT("http://118.26.164.109:81/uploads/face/",jl_user.user_face)  as from_user_face'))
+            $list =$sql ->select('comment_id','course_id', 'parent_id', 'content', 'from_user','from_user_name','to_user','to_user_name','created_at','praise_count as praise_num',DB::raw('CONCAT("http://118.26.164.109:81/uploads/face/",jl_user.user_face)  as from_user_face'))
                 ->leftJoin('user','user.user_id','courses_comments.from_user')
                 ->skip(($page_index - 1) * $page_number)->take($page_number)->get()->toArray();
             //查询列表的回复列表
             foreach($list as $key=>$value){
                 //此评论的点赞数量
-                $list[$key]['praise_num'] = Praises::where('comment_id',$value['comment_id'])->count();
+//                $list[$key]['praise_num'] = Praises::where('comment_id',$value['comment_id'])->count();
                 //当前登录用户是否已经点过赞
                 if($request->input('login_user')){
                     $exits = Praises::where('from_user',$request->input('login_user'))->where('comment_id',$value['comment_id'])->exists();
@@ -833,7 +833,7 @@ class CoursesController extends Controller{
         $result = array();
         if($parent_id){
             $result = Comments::where('parent_id',$parent_id)
-                ->select('courses_comments.content','courses_comments.from_user','courses_comments.from_user_name','courses_comments.to_user','courses_comments.to_user_name','courses_comments.created_at',DB::raw('CONCAT("http://118.26.164.109:81/uploads/face/",jl_user.user_face)  as from_user_face'))
+                ->select('courses_comments.comment_id','courses_comments.content','courses_comments.from_user','courses_comments.from_user_name','courses_comments.to_user','courses_comments.to_user_name','courses_comments.created_at','courses_comments.praise_count as praise_num',DB::raw('CONCAT("http://118.26.164.109:81/uploads/face/",jl_user.user_face)  as from_user_face'))
                 ->leftJoin('user','user.user_id','courses_comments.from_user')->get()->toArray();
         }
         return $result;
