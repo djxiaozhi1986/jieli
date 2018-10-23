e97f20a90bd077e0adc259b960b872b6f8057c4d
GavinSir <djxiaozhi1986@163.com>
2018-10-23 16:14:50 +0800

    up

--- config/C.php
+++ config/C.php
@@ -2,12 +2,12 @@
 
 return array(
     'ROOT_PATH'             =>      dirname(__DIR__),
-     'DOMAIN'                =>     'http://api.php.tiennet.com/',
+     'DOMAIN'                =>     'https://api.php.tiennet.com/',
 //    'DOMAIN'                =>     'http://jl.cn/',
     'IMG_URL'               =>      'uploads/images/',
     'FILE_URL'              =>      'uploads/files/',
     'UPLOAD_TMP'            =>      'uploads/tmp/',
     'DEFAULT_AVATOR'        =>      'default_avator.png',
-    'API_URL'               =>      'http://api.java.tiennet.com',
+    'API_URL'               =>      'https://api.java.tiennet.com',
     'MASSAGE_URL'           =>      'http://106.ihuyi.cn/webservice/sms.php?method=Submit',
 );
